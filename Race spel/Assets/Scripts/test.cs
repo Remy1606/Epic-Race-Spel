be32305ee2e04625205e9d2f623188cb4fc3af9f
Remy van Tongeren <27454@edu.msa.nl>
2026-01-13 14:38:55 +0100

    Test script
    een andere manier van snelheid bedenken en testen

---
+++ Race spel/Assets/Scripts/test.cs
@@ -0,0 +1,150 @@
+using UnityEngine;
+using TMPro;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+
+
+{
+    public float speed = 20f;
+    public float turnSpeed;
+    public float forwardAcceleration = 0.0f;
+    public float maxAcceleration;
+    private float horizontalInput;
+    public float forwardInput;
+    public float slowDownRate;
+    public TextMeshProUGUI toerenText;
+    public TextMeshProUGUI GearText;
+    public float toerenPerMinuut;
+    public int currentGear = 1;
+    public int inputToRpm;
+    public int maxRPM = 8000;
+    public int minRPM = 900;
+    public int RPMFactor;
+    public float strengh;
+    public bool drivable = true;
+    public int maxGear = 5;
+
+    void SetText()
+    {
+        toerenText.text = toerenPerMinuut.ToString("0");
+        GearText.text = currentGear.ToString();
+    }
+
+    void Start()
+    {
+        SetText();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        slowDownRate = (float)(0.5 * (forwardAcceleration * forwardAcceleration));
+        RPMFactor = maxRPM- minRPM;
+
+        if (Input.GetKeyDown(KeyCode.Period) && currentGear != maxGear)
+        {
+            currentGear += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Comma) && currentGear != 0)
+        {
+            currentGear -= 1;
+        }
+
+        
+        forwardAcceleration = SnelheidUpdate();
+        turnSpeed = SturenSnelheidUpdate();
+        toerenPerMinuut = EngineRPM();
+        drivable = Drivability();
+
+        SetText();
+        // foward based on user input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardAcceleration);
+        // rotate based on user input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+    float EngineRPM()
+    {
+        toerenPerMinuut += RPMFactor * forwardInput * Time.deltaTime;
+
+        if (toerenPerMinuut >= maxRPM)
+        {
+            toerenPerMinuut = toerenPerMinuut - 100;
+        }
+
+        if (toerenPerMinuut <= minRPM)
+        {
+            toerenPerMinuut = minRPM;
+        }
+
+        if (currentGear != 0)
+        { 
+            strengh = (float)(maxGear / currentGear);
+            inputToRpm = RPMFactor / currentGear;
+            maxAcceleration = currentGear;
+        }
+        else
+        {
+            strengh = 0;
+            inputToRpm = RPMFactor;
+            maxAcceleration = 1;
+        }
+
+        return toerenPerMinuut;
+    }
+
+    float SnelheidUpdate()
+    {
+        if (drivable)
+        {
+            forwardAcceleration = (toerenPerMinuut - 900) / RPMFactor;            
+        }
+
+        if (forwardInput == 0)
+        {
+            forwardAcceleration -= slowDownRate * Time.deltaTime;
+        }
+
+        if (forwardInput < 0 && forwardAcceleration <= 0)
+        {
+            forwardAcceleration = 0;
+        }
+
+        if (forwardAcceleration >= maxAcceleration)
+        {
+            forwardAcceleration = maxAcceleration;
+        }
+        
+        if (forwardAcceleration < 0.15 && forwardInput == 0)
+        {
+            forwardAcceleration = 0;
+        }
+
+        return forwardAcceleration;
+    }
+
+
+    float SturenSnelheidUpdate()
+    {
+        if (forwardAcceleration == 0)
+        {
+            turnSpeed = 0;
+        }
+        else
+        {
+            turnSpeed = (float)(-(10 * forwardAcceleration) + 80);
+        }
+        return turnSpeed;
+    }
+
+    // optimaliseren: kan niet remmen in hogere versnelling want drivable wordt false
+    bool Drivability()
+    {
+        
+
+        return drivable;
+    }
+
+}
